5c68504192e794a8875190b89738139700f73992
Vanessa <vtorres2004.1211@gmail.com>
2025-11-12 20:22:46 -0500

    config cosecha

--- app/Models/Cosecha.php
+++ app/Models/Cosecha.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cosecha extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'cultivo_id',
+        'fecha',
+        'cantidad',
+        'unidad',
+        'calidad',
+        'mermas',
+        'notas',
+    ];
+
+    protected $casts = [
+        'fecha' => 'date',
+    ];
+
+    public function cultivo()
+    {
+        return $this->belongsTo(Cultivo::class);
+    }
 }
