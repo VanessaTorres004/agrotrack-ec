32aceba7ee90cd58bd5b763e9a04f26339fbe8b8
Vanessa <vtorres2004.1211@gmail.com>
2025-11-12 20:36:30 -0500

    config ganado

--- app/Policies/GanadoPolicy.php
+++ app/Policies/GanadoPolicy.php
@@ -3,14 +3,22 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\Ganado;
 
 class GanadoPolicy
 {
-    /**
-     * Create a new policy instance.
-     */
-    public function __construct()
+    public function view(User $user, Ganado $ganado): bool
     {
-        //
+        return $user->rol === 'administrador' || $ganado->finca->user_id === $user->id;
+    }
+
+    public function update(User $user, Ganado $ganado): bool
+    {
+        return $user->rol === 'administrador' || $ganado->finca->user_id === $user->id;
+    }
+
+    public function delete(User $user, Ganado $ganado): bool
+    {
+        return $user->rol === 'administrador' || $ganado->finca->user_id === $user->id;
     }
 }
