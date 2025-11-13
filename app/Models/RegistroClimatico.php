a6555b02e5d19041fcf4904e7c8e737610a559f6
Vanessa <vtorres2004.1211@gmail.com>
2025-11-12 20:30:54 -0500

    config registro

--- app/Models/RegistroClimatico.php
+++ app/Models/RegistroClimatico.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class RegistroClimatico extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'finca_id',
+        'fecha',
+        'temperatura_min',
+        'temperatura_max',
+        'humedad',
+        'precipitacion',
+        'eventos',
+        'factor_clima',
+    ];
+
+    protected $casts = [
+        'fecha' => 'date',
+    ];
+
+    public function finca()
+    {
+        return $this->belongsTo(Finca::class);
+    }
 }
+
