aa021d724a21c2da150c5cde3a0e679d740fc311
Vanessa <vtorres2004.1211@gmail.com>
2025-11-12 20:33:03 -0500

    config vacuna

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Venta extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'cultivo_id',
+        'fecha',
+        'comprador',
+        'cantidad',
+        'unidad',
+        'precio_unitario',
+        'total',
+        'notas',
+    ];
+
+    protected $casts = [
+        'fecha' => 'date',
+    ];
+
+    public function cultivo()
+    {
+        return $this->belongsTo(Cultivo::class);
+    }
 }
+
