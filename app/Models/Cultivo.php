f9bdf6ff762f768940b9be00885bc519d45f9f7e
Vanessa <vtorres2004.1211@gmail.com>
2025-11-12 20:23:17 -0500

    config cultivo

--- app/Models/Cultivo.php
+++ app/Models/Cultivo.php
@@ -2,9 +2,66 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cultivo extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'finca_id',
+        'nombre',
+        'variedad',
+        'area',
+        'fecha_siembra',
+        'fecha_cosecha_estimada',
+        'estado',
+        'notas',
+    ];
+
+    protected $casts = [
+        'fecha_siembra' => 'date',
+        'fecha_cosecha_estimada' => 'date',
+    ];
+
+    public function finca()
+    {
+        return $this->belongsTo(Finca::class);
+    }
+
+    public function actualizaciones()
+    {
+        return $this->hasMany(Actualizacion::class);
+    }
+
+    public function cosechas()
+    {
+        return $this->hasMany(Cosecha::class);
+    }
+
+    public function ventas()
+    {
+        return $this->hasMany(Venta::class);
+    }
+
+    public function indicadores()
+    {
+        return $this->hasMany(Indicador::class);
+    }
+
+    public function alertas()
+    {
+        return $this->hasMany(Alerta::class);
+    }
+
+    public function getIdcActualAttribute()
+    {
+        return $this->indicadores()->latest()->first()?->idc ?? 0;
+    }
+
+    public function getClasificacionActualAttribute()
+    {
+        return $this->indicadores()->latest()->first()?->clasificacion ?? 'sin_datos';
+    }
 }
