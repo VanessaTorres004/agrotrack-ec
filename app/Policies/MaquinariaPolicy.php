2f03c5a1e645dd8c2bfdacef879817bc7ba1c57d
Vanessa <vtorres2004.1211@gmail.com>
2025-11-12 20:36:54 -0500

    config maquinaria

--- app/Policies/MaquinariaPolicy.php
+++ app/Policies/MaquinariaPolicy.php
@@ -3,14 +3,22 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\Maquinaria;
 
 class MaquinariaPolicy
 {
-    /**
-     * Create a new policy instance.
-     */
-    public function __construct()
+    public function view(User $user, Maquinaria $maquinaria): bool
     {
-        //
+        return $user->rol === 'administrador' || $maquinaria->finca->user_id === $user->id;
+    }
+
+    public function update(User $user, Maquinaria $maquinaria): bool
+    {
+        return $user->rol === 'administrador' || $maquinaria->finca->user_id === $user->id;
+    }
+
+    public function delete(User $user, Maquinaria $maquinaria): bool
+    {
+        return $user->rol === 'administrador' || $maquinaria->finca->user_id === $user->id;
     }
 }
