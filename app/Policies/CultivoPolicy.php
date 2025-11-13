402205393a4ca174fa67313d27fa968d2416359f
Vanessa <vtorres2004.1211@gmail.com>
2025-11-12 20:36:03 -0500

    config cultivo

--- app/Policies/CultivoPolicy.php
+++ app/Policies/CultivoPolicy.php
@@ -3,14 +3,22 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\Cultivo;
 
 class CultivoPolicy
 {
-    /**
-     * Create a new policy instance.
-     */
-    public function __construct()
+    public function view(User $user, Cultivo $cultivo): bool
     {
-        //
+        return $user->rol === 'administrador' || $cultivo->finca->user_id === $user->id;
+    }
+
+    public function update(User $user, Cultivo $cultivo): bool
+    {
+        return $user->rol === 'administrador' || $cultivo->finca->user_id === $user->id;
+    }
+
+    public function delete(User $user, Cultivo $cultivo): bool
+    {
+        return $user->rol === 'administrador' || $cultivo->finca->user_id === $user->id;
     }
 }
