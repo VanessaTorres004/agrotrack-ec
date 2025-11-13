7c2d829b196c6b40b9f2dd2240ff280e7498d707
Vanessa <vtorres2004.1211@gmail.com>
2025-11-12 20:23:48 -0500

    config finca

--- app/Models/Finca.php
+++ app/Models/Finca.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Finca extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'nombre',
+        'ubicacion',
+        'area_total',
+        'provincia',
+        'canton',
+        'descripcion',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function cultivos()
+    {
+        return $this->hasMany(Cultivo::class);
+    }
+
+    public function registrosClimaticos()
+    {
+        return $this->hasMany(RegistroClimatico::class);
+    }
 }
