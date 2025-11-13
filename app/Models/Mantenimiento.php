cc234e5a6d174e457c8a6db13ebd688ed19f75b9
Vanessa <vtorres2004.1211@gmail.com>
2025-11-12 20:25:13 -0500

    config mantenimiento

--- app/Models/Mantenimiento.php
+++ app/Models/Mantenimiento.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Mantenimiento extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'maquinaria_id',
+        'fecha_mantenimiento',
+        'tipo',
+        'descripcion',
+        'costo',
+        'tecnico',
+    ];
+
+    protected $casts = [
+        'fecha_mantenimiento' => 'date',
+        'costo' => 'decimal:2',
+    ];
+
+    public function maquinaria()
+    {
+        return $this->belongsTo(Maquinaria::class);
+    }
 }
