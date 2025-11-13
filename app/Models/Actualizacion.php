e3e6b61a3b9e75cb41329b4ff61ade001d53da44
Vanessa <vtorres2004.1211@gmail.com>
2025-11-12 20:21:29 -0500

    config actualización

--- app/Models/Actualizacion.php
+++ app/Models/Actualizacion.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Actualizacion extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'actualizaciones';
+
+    protected $fillable = [
+        'cultivo_id',
+        'fecha',
+        'tipo',
+        'observaciones',
+        'accion_tomada',
+    ];
+
+    protected $casts = [
+        'fecha' => 'date',
+    ];
+
+    public function cultivo()
+    {
+        return $this->belongsTo(Cultivo::class);
+    }
 }
