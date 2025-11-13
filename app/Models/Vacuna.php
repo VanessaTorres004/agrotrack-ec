3dcb3386de5a08b5bb6d3221508ce07ea59e80d3
Vanessa <vtorres2004.1211@gmail.com>
2025-11-12 20:32:37 -0500

    config vacuna

--- app/Models/Vacuna.php
+++ app/Models/Vacuna.php
@@ -2,9 +2,49 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vacuna extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'ganado_id',
+        'tipo_vacuna',
+        'fecha_aplicacion',
+        'proxima_dosis',
+        'veterinario',
+        'observaciones',
+        'estado',
+    ];
+
+    protected $casts = [
+        'fecha_aplicacion' => 'date',
+        'proxima_dosis' => 'date',
+    ];
+
+    public function ganado()
+    {
+        return $this->belongsTo(Ganado::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($vacuna) {
+            if ($vacuna->proxima_dosis) {
+                $diasRestantes = now()->diffInDays($vacuna->proxima_dosis, false);
+                
+                if ($diasRestantes < 0) {
+                    $vacuna->estado = 'vencida';
+                } elseif ($diasRestantes <= 7) {
+                    $vacuna->estado = 'proxima';
+                } else {
+                    $vacuna->estado = 'aplicada';
+                }
+            }
+        });
+    }
 }
