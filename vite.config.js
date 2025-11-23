45d6bd362afc1715212ece210251b38856cdc973
DayaPau <dayana.vallejos.casanova@udla.edu.ec>
2025-11-23 01:35:48 -0500

    config railway

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-});
+export default {
+    css: {
+        minify: false
+    }
+};
